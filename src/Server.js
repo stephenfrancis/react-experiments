c416a5a869dd76c63d0ba94eab56585e5ffa55a8
Stephen Francis <stephen.d.francis@gmail.com>
2020-10-19 15:38:05 +0100

    tweak build and serving for heroku

--- src/Server.js
+++ src/Server.js
@@ -1,9 +1,12 @@
 const Koa = require("koa");
+const KoaStatic = require("koa-static")
 const ListRedux = require("./list_redux/Server");
 
 const app = new Koa();
 
+app.use(KoaStatic("dist/", {
+  maxAge: "1y",
+}))
 app.use(ListRedux);
-
 
 app.listen(process.env.PORT || 3000);
